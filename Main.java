fcf0532e8b7ce9d99ef24a92c7ea291e0ffe48e8
Natasha Sarkar <nat41575@gmail.com>
2017-09-05 16:11:30 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -59,12 +59,12 @@ public class Main {
 
 	public static void clear_screen() {
 		System.out.print("\033[H\033[2J");  
-    	System.out.flush();  
+    		System.out.flush();  
 	}
 
 	public static void delay(int ms) {
 		try {
-    		Thread.sleep(ms);
+    			Thread.sleep(ms);
 		} catch(InterruptedException ex) {
   			Thread.currentThread().interrupt();
 		}
